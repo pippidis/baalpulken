c8f3132a8436f455b79822d40c84041eb7a822f3
Johannes Lorentzen <pippidis@gmail.com>
2023-03-07 22:56:18 +0100

    Problemer med å

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/bålpulken-icon.svg`, // This path is relative to the root of the site.
+        icon: `src/images/baalpulken-icon.svg`, // This path is relative to the root of the site.
       },
     },
   ],
